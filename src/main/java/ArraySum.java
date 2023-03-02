c9f883954134fd3213ca7dbd79fad930629e1d3e
meryemselicioglu <meryemselici2000@gmail.com>
2023-03-02 03:48:02 +0000

    commit changes

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -7,7 +7,13 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
+        int result = 0;
 
-        return 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            result += arr[i];
+        }
+
+        return result;
     }
 }
